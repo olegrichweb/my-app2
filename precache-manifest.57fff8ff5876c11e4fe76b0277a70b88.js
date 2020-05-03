d6904a6983d68f9cf000e0f1ce95ecc441999d5f
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-05-03 15:47:10 +0300

    Updates

---
+++ precache-manifest.57fff8ff5876c11e4fe76b0277a70b88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a9beced80def0bd99ca92e81bed07a",
+    "url": "/my-app2/index.html"
+  },
+  {
+    "revision": "ee132b47b7d72823972b",
+    "url": "/my-app2/static/css/main.ed397576.chunk.css"
+  },
+  {
+    "revision": "0654671c12a9d2237320",
+    "url": "/my-app2/static/js/2.ad1d4999.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app2/static/js/2.ad1d4999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee132b47b7d72823972b",
+    "url": "/my-app2/static/js/main.1291d8c0.chunk.js"
+  },
+  {
+    "revision": "1ce9238c0af5e2a8da0c",
+    "url": "/my-app2/static/js/runtime-main.95d85c14.js"
+  },
+  {
+    "revision": "f3757028ff67a76a48fec3a85a50ebc8",
+    "url": "/my-app2/static/media/iconfinder_1_759442.f3757028.svg"
+  },
+  {
+    "revision": "2869e16262112c444d20bb0bc231da78",
+    "url": "/my-app2/static/media/iconfinder_glyph-18_3403715.2869e162.svg"
+  },
+  {
+    "revision": "8a7c6e6ccbd887058b1731035dfc1e24",
+    "url": "/my-app2/static/media/iconfinder_m-17_4230547.8a7c6e6c.svg"
+  }
+]);
